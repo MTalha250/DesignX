50160ed0533d747a6a50504003ef875638bc63ae
Muhammad Talha <talhabinay@gmail.com>
2023-06-12 09:03:01 +0500

    update

--- Client/src/PageComponents/Showcase.js
+++ Client/src/PageComponents/Showcase.js
@@ -6,21 +6,21 @@ const Showcase = () => {
   useEffect(() => {
     const getData = async () => {
       const response = await axios.get(
-        process.env.REACT_APP_PATH + "showcaase/get"
+        process.env.REACT_APP_PATH + "showcase/get"
       );
       setData(response.data);
     };
-
     getData();
   }, []);
+
   return (
     <div className="my-5 px-10">
-      <h1>Showcase</h1>
-      <div className="grid grid-cols-2">
+      <h1 className="text-4xl text-center font-bold">Showcase</h1>
+      <div className="grid grid-cols-2 gap-10">
         {data.map((d) => (
-          <div>
+          <div className="p-10">
             <img src={process.env.REACT_APP_PATH + d.imgs[0]} alt="" />
-            <p>{d.name}</p>
+            <p className="text-3xl font-bold text-center">{d.name}</p>
           </div>
         ))}
       </div>
