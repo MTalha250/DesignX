24463492a8e40d298808c69323f06b269b91fc1d
Muhammad Talha <talhabinay@gmail.com>
2023-06-12 08:49:49 +0500

    update

--- Client/src/PageComponents/Showcase.js
+++ Client/src/PageComponents/Showcase.js
@@ -1,7 +1,31 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 const Showcase = () => {
-  return <div>Showcase</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(
+        process.env.REACT_APP_PATH + "showcaase/get"
+      );
+      setData(response.data);
+    };
+
+    getData();
+  }, []);
+  return (
+    <div className="my-5 px-10">
+      <h1>Showcase</h1>
+      <div className="grid grid-cols-2">
+        {data.map((d) => (
+          <div>
+            <img src={process.env.REACT_APP_PATH + d.imgs[0]} alt="" />
+            <p>{d.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Showcase;
