d3ec3c5b677ed2a38ee9b327759b9901b31a6fac
Muhammad Talha <talhabinay@gmail.com>
2023-06-12 06:40:55 +0500

    update

--- Client/src/SubComponents/Account/Admin/Quotes.js
+++ Client/src/SubComponents/Account/Admin/Quotes.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 import { Link } from "react-router-dom";
+import CloseIcon from "@mui/icons-material/Close";
 
 const Quotes = () => {
   const [data, setData] = useState([]);
@@ -14,12 +16,27 @@ const Quotes = () => {
     getData();
   }, []);
 
+  const handleDelete = async (id) => {
+    const response = await axios.delete(
+      process.env.REACT_APP_PATH + `quotes/delete/${id}`
+    );
+    toast(response.data.message);
+    const getData = await axios.get(process.env.REACT_APP_PATH + "quotes/get");
+    setData(getData.data);
+  };
+
   return (
     <div className="py-5 px-10">
       <h1 className="text-center text-2xl md:text-3xl font-bold">All Quotes</h1>
       <div className="my-5 grid grid-cols-2 gap-5">
         {data?.map((d) => (
-          <div className="p-3 border border-black rounded-xl">
+          <div className="relative p-3 border border-black rounded-xl">
+            <button
+              className="absolute right-2 top-2"
+              onClick={() => handleDelete(d._id)}
+            >
+              <CloseIcon />
+            </button>
             <p>Name: {d.name}</p>
             <p>Email: {d.email}</p>
             <p>Number: {d.no}</p>
